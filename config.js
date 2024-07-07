e8f497f1e6e28479ee417009e44ed4f8c0eb890c
Mohamed0120160 <142312063+Mohamed0120160@users.noreply.github.com>
2024-07-07 11:41:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,153 @@
+import {watchFile, unwatchFile} from 'fs';
+import chalk from 'chalk';
+import {fileURLToPath} from 'url';
+import fs from 'fs'; 
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
+
+global.owner = [
+  ['201201609786', '꧁✿[𝒢ℴ𝒿ℴ]✿꧂', true],['201201609786', '꧁✿[𝒢ℴ𝒿ℴ]✿꧂', true]
+];
+
+global.xaxa = 'Lona'
+global.suittag = ['201201609786', '201201609786'];
+global.prems = ['201201609786', '201201609786'];
+
+
+(function (_0x44283f, _0x13d0d2) {
+    var _0x46a3b5 = _0x44283f();
+    while (!![]) {
+        try {
+            var _0x35673b = -parseInt(_0x21bf(0x5)) / 0x1 * (-parseInt(_0x21bf(0x14)) / 0x2) + parseInt(_0x21bf(0x17)) / 0x3 * (parseInt(_0x21bf(0x9)) / 0x4) + -parseInt(_0x21bf(0x12)) / 0x5 + -parseInt(_0x21bf(0xd)) / 0x6 * (-parseInt(_0x21bf(0x1a)) / 0x7) + -parseInt(_0x21bf(0xc)) / 0x8 + -parseInt(_0x21bf(0x4)) / 0x9 * (-parseInt(_0x21bf(0x1e)) / 0xa) + -parseInt(_0x21bf(0x8)) / 0xb * (parseInt(_0x21bf(0x11)) / 0xc);
+            if (_0x35673b === _0x13d0d2) {
+                break;
+            } else {
+                _0x46a3b5['push'](_0x46a3b5['shift']());
+            }
+        } catch (_0x3d4a9f) {
+            _0x46a3b5['push'](_0x46a3b5['shift']());
+        }
+    }
+}(_0x428a, 0x81dc4));
+
+function _0x21bf(_0x4536d9, _0x178b79) {
+    var _0x1c5a51 = _0x428a();
+    _0x21bf = function (_0x48cddb, _0x117664) {
+        _0x48cddb = _0x48cddb - 0x0;
+        var _0x428aa0 = _0x1c5a51[_0x48cddb];
+        return _0x428aa0;
+    };
+    return _0x21bf(_0x4536d9, _0x178b79);
+}
+var _0x48cddb = (function () {
+    var _0x5ee028 = !![];
+    return function (_0x471b61, _0x2b4492) {
+        var _0x42076a = _0x5ee028 ? function () {
+            if (_0x2b4492) {
+                var _0x1eea67 = _0x2b4492[_0x21bf(0x2)](_0x471b61, arguments);
+                _0x2b4492 = null;
+                return _0x1eea67;
+            }
+        } : function () {};
+        _0x5ee028 = ![];
+        return _0x42076a;
+    };
+}());
+var _0x1c5a51 = _0x48cddb(this, function () {
+    var _0x1126db;
+    try {
+        var _0x14614b = Function(_0x21bf(0x6) + '{}.constructor(\"return this\")( )' + ');');
+        _0x1126db = _0x14614b();
+    } catch (_0x351ffa) {
+        _0x1126db = window;
+    }
+    var _0x215617 = _0x1126db[_0x21bf(0x10)] = _0x1126db[_0x21bf(0x10)] || {};
+    var _0x43cecc = [_0x21bf(0x19), 'warn', _0x21bf(0xe), 'error', _0x21bf(0x20), _0x21bf(0x7), _0x21bf(0x18)];
+    for (var _0x5bd2dc = 0x0; _0x5bd2dc < _0x43cecc[_0x21bf(0xa)]; _0x5bd2dc++) {
+        var _0x26be7d = _0x48cddb[_0x21bf(0x1)][_0x21bf(0x3)][_0x21bf(0x16)](_0x48cddb);
+        var _0x1347e8 = _0x43cecc[_0x5bd2dc];
+        var _0x1b8503 = _0x215617[_0x1347e8] || _0x26be7d;
+        _0x26be7d['__proto__'] = _0x48cddb[_0x21bf(0x16)](_0x48cddb);
+        _0x26be7d[_0x21bf(0x13)] = _0x1b8503[_0x21bf(0x13)][_0x21bf(0x16)](_0x1b8503);
+        _0x215617[_0x1347e8] = _0x26be7d;
+    }
+});
+_0x1c5a51();
+global[_0x21bf(0x0)] = '❄️ ꧁✿[𝒢ℴ𝒿ℴ]✿꧂ 🧿\x0aꨁ https://wa.me/+201201609786 ꨁ\x0a🔥 ꧁✿[𝒢ℴ𝒿ℴ]✿꧂ 🧿\x0aꨁ https://wa.me/+201201609786 ꨁ';
+global[_0x21bf(0x1b)] = '𝑺𝛩𝐾𝑈𝑁𝐴 - 𝑉𝐸𝑁𝛩𝑀 ';
+global[_0x21bf(0x1c)] = _0x21bf(0xb);
+global['wm'] = _0x21bf(0xf);
+global['titulowm'] = _0x21bf(0x1f);
+
+function _0x428a() {
+    var _0x53da88 = ['bind', '6dprKij', 'trace', 'log', '56sZQCCY', 'packname', 'author', '★LoNa Bot★', '320ATjCVW', '🤖 LoNa Bot 🤖', 'exception', '꧁✿[𝒢ℴ𝒿ℴ]✿꧂', 'constructor', 'apply', 'prototype', '28422sTEpQW', '1015167mUzCjV', 'return (function() ', 'table', '33aSKDvQ', '1976368gYkUtU', 'length', '★LoNa Bot★', '6397984ImDGHu', '119550OivDxD', 'info', '★LoNa Bot★', 'console', '3268884qPBkke', '574650ACwiXW', 'toString', '2Cmqyio', 'titulowm2'];
+    _0x428a = function () {
+        return _0x53da88;
+    };
+    return _0x428a();
+}
+global[_0x21bf(0x15)] = '乂 LoNa Bot 乂';
+global['igfg'] = _0x21bf(0x1d);
+global.wait = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
+
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen3 = fs.readFileSync('./Menu3.png');
+
+global.mods = [];
+
+//* *******Tiempo***************
+global.d = new Date(new Date + 3600000);
+global.locale = 'ar';
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
+global.fecha = d.toLocaleDateString('ar', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('ar', {month: 'long'});
+global.año = d.toLocaleDateString('ar', {year: 'numeric'});
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+//* ****************************
+global.wm2 = `▸ ${dia} ${fecha}\n▸ Lona Bot`;
+global.gt = '★Lona Bot★';
+global.mysticbot = '★Lona Bot★';
+global.md = 'https://whatsapp.com/channel/0029VadTAXd9xVJXSkaqtQ2X';
+global.mysticbot = 'https://whatsapp.com/channel/0029VadTAXd9xVJXSkaqtQ2X';
+global.waitt = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
+global.waittt = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▰▰▱▱▱*';
+global.waitttt = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▰▰▰▱▱*';
+global.nomorown = '20103324135';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+global.cmenut = '❖––––––『';
+global.cmenub = '┊✦ ';
+global.cmenuf = '╰━═┅═━––––––๑\n';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.dmenut = '*❖─┅──┅〈*';
+global.dmenub = '*┊»*';
+global.dmenub2 = '*┊*';
+global.dmenuf = '*╰┅────────┅✦*';
+global.htjava = '⫹⫺';
+global.htki = '*⭑•̩̩͙⊱•••• ☪*';
+global.htka = '*☪ ••••̩̩͙⊰•⭑*';
+global.comienzo = '• • ◕◕════';
+global.fin = '════◕◕ • •';
+global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}`; // Asia/Jakarta
+global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`;// America/Los_Angeles
+global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.multiplier = 99;
+global.flaaa = [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+];
+//* ************************
+
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright('Update \'config.js\''));
+  import(`${file}?update=${Date.now()}`);
+});
